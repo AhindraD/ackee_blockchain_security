fe7d5b717f07ad925f400f9e3b235b7db3aca3ed
Ahindra D. <official.ahindra@gmail.com>
2024-10-31 01:44:44 +0530

    task

--- 01.school_of_solana/a_rust_intro/src/option_result.rs
+++ 01.school_of_solana/a_rust_intro/src/option_result.rs
@@ -4,6 +4,7 @@ fn divide(a: f32, b: f32) -> Result<f32, String> {
     //     Ok(T),
     //     Err(E),
     // }
+    //Use Result when errors need handling, allowing you to carry information about what went wrong.
     if b == 0.0 {
         Err(String::from("cannot devide by 0"))
     } else {
@@ -12,6 +13,11 @@ fn divide(a: f32, b: f32) -> Result<f32, String> {
 }
 
 fn find_char(s: &str, c: char) -> Option<usize> {
+    // enum Option<T> {
+    //     Some(T),
+    //     None,
+    // }
+    // Use Option when absence of a value is expected and doesn't necessarily represent an error.
     for (i, ch) in s.chars().enumerate() {
         if ch == c {
             return Some(i);
